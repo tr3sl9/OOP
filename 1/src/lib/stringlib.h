87e7d71b1c22d51cdf684fef95319732c1916691
simonenko.mv <tr3sl9@DESKTOP-FPKAD0O.>
2025-09-04 19:53:34 +0300

    feat: add C implemetation

---
+++ 1/src/lib/stringlib.h
@@ -0,0 +1,8 @@
+#ifndef STRINGLIB_H
+#define STRINGLIB_H
+#include "../lib/stringlib.h"
+
+char* read_string_console(const char * prompt);
+char* read_process(FILE * const file, char * const source);
+
+#endif
