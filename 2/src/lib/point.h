4b3c1ead8a873cb656cbda7b2ee7dd88f8622fe4
simonenko.mv <tr3sl9@DESKTOP-FPKAD0O.>
2025-10-16 23:36:46 +0300

    refactor: methods

--- 2/src/lib/point.h
+++ 2/src/lib/point.h
@@ -1,38 +1,121 @@
+/**
+ * @file point.h
+ * @brief Заголовочный файл класса Point для работы с точками на плоскости
+ */
+
 #ifndef POINT_H
 #define POINT_H
 
 #include <iostream>
 #include <cmath>
 
+/**
+ * @class Point
+ * @brief Класс для представления точки на двумерной плоскости
+ * 
+ * Класс Point представляет точку с координатами (x, y) и предоставляет
+ * методы для работы с точками: вычисление расстояний, арифметические
+ * операции, поворот и ввод/вывод.
+ */
 class Point {
 private: 
-    float x, y;
+    float x; ///< Координата X точки
+    float y; ///< Координата Y точки
 
 public:
-    // Конструкторы
+    /**
+     * @brief Конструктор по умолчанию
+     * 
+     * Создает точку с координатами (0, 0).
+     */
     Point();
+    
+    /**
+     * @brief Конструктор с параметрами
+     * @param x Координата X
+     * @param y Координата Y
+     */
     Point(float x, float y);
+    
+    /**
+     * @brief Конструктор с массивом координат
+     * @param coords Массив из двух элементов [x, y]
+     */
     Point(const float coords[2]);
     
-    // Геттеры
+    /**
+     * @brief Получить координату X
+     * @return Координата X точки
+     */
     float getX() const;
+
+    /**
+     * @brief Получить координату Y
+     * @return Координата Y точки
+     */
     float getY() const;
 
-    // Сеттеры
+    /**
+     * @brief Установить координату X
+     * @param x Новое значение координаты X
+     */
     void setX(float x);
+    
+    /**
+     * @brief Установить координату Y
+     * @param y Новое значение координаты Y
+     */
     void setY(float y);
     
-    // Методы
+    /**
+     * @brief Вычислить расстояние до другой точки
+     * @param other Другая точка
+     * @return Расстояние между точками
+     */
     float distanceTo(const Point& other) const;
+    
+    /**
+     * @brief Повернуть точку на 90° по часовой стрелке относительно начала координат
+     * 
+     * Преобразование: (x, y) -> (y, -x)
+     */
     void rotate90Clockwise();
 
-    // Операторы
+    /**
+     * @brief Оператор сложения точек
+     * @param other Другая точка
+     * @return Новая точка с суммой координат
+     */
     Point operator+(const Point& other) const;
+    
+    /**
+     * @brief Оператор вычитания точек
+     * @param other Другая точка
+     * @return Новая точка с разностью координат
+     */
     Point operator-(const Point& other) const;
+    
+    /**
+     * @brief Оператор деления координат на число
+     * @param divisor Делитель
+     * @return Новая точка с координатами, поделенными на divisor
+     */
     Point operator/(float divisor) const;
     
-    // Ввод/вывод
+    /**
+     * @brief Оператор ввода из потока
+     * @param is Входной поток
+     * @param point Точка для чтения
+     * @return Ссылка на поток
+     */
     friend std::istream& operator>>(std::istream& is, Point& point);
+    
+    /**
+     * @brief Оператор вывода в поток
+     * @param os Выходной поток
+     * @param point Точка для вывода
+     * @return Ссылка на поток
+     */
     friend std::ostream& operator<<(std::ostream& os, const Point& point);
 };
 
--- 2/src/lib/polygon.h
+++ 2/src/lib/polygon.h
@@ -1,3 +1,8 @@
+/**
+ * @file polygon.h
+ * @brief Заголовочный файл класса Polygon для работы с многоугольниками
+ */
+
 #ifndef POLYGON_H
 #define POLYGON_H
 
@@ -8,30 +13,70 @@
 
 #include "point.h"
 
+/**
+ * @class Polygon
+ * @brief Класс для представления многоугольника как набора точек
+ * 
+ * Класс Polygon представляет многоугольник как массив точек (вершин).
+ * Предоставляет методы для создания, модификации и геометрических
+ * преобразований многоугольников.
+ */
 class Polygon {
 private:
-    size_t point_count_;
-    Point* point_array_;
+    size_t point_count_ = 0; ///< Количество вершин многоугольника
+    Point* point_array_ = nullptr; ///< Массив вершин многоугольника
     
 public:
+    /**
+     * @brief Конструктор по умолчанию
+     * 
+     * Создает пустой многоугольник (0 вершин).
+     */
     Polygon() = default;
 
+    /**
+     * @brief Конструктор с одной вершиной
+     * @param vertex Единственная вершина многоугольника
+     */
     Polygon(const Point& vertex): point_count_(1), point_array_(new Point[point_count_]) {
         point_array_[point_count_ - 1] = vertex;
     }
 
+    /**
+     * @brief Конструктор с заданным количеством вершин
+     * @param vertex_count Количество вершин (создаются точки (0,0))
+     */
     Polygon(const size_t vertex_count) : point_count_(vertex_count), point_array_(new Point[point_count_]) {}
 
+    /**
+     * @brief Конструктор с массивом вершин
+     * @param vertex_count Количество вершин
+     * @param vertex_array Массив вершин для копирования
+     */
     Polygon(const size_t vertex_count, const Point* const vertex_array) : point_count_(vertex_count), point_array_(new Point[point_count_]) {
         std::copy_n(vertex_array, point_count_, point_array_);
     }
 
+    /**
+     * @brief Деструктор
+     * 
+     * Освобождает память, выделенную под массив вершин.
+     */
     ~Polygon() { delete[] point_array_; }
 
+    /**
+     * @brief Конструктор копирования
+     * @param other Другой многоугольник для копирования
+     */
     Polygon(const Polygon& other): point_count_(other.point_count_), point_array_(new Point[point_count_]) {
         std::copy_n(other.point_array_, point_count_, point_array_);
     }
 
+    /**
+     * @brief Оператор присваивания (копирование)
+     * @param other Другой многоугольник
+     * @return Ссылка на текущий объект
+     */
     Polygon& operator=(const Polygon& other) {
         if (this != &other) {
             Polygon save_place(other);
@@ -44,11 +89,20 @@ public:
         return *this;
     }
     
+    /**
+     * @brief Конструктор перемещения
+     * @param other Другой многоугольник для перемещения
+     */
     Polygon(Polygon&& other) noexcept : point_count_(other.point_count_), point_array_(other.point_array_) {
         other.point_count_ = 0;
         other.point_array_ = nullptr;
     }
 
+    /**
+     * @brief Оператор присваивания (перемещение)
+     * @param other Другой многоугольник
+     * @return Ссылка на текущий объект
+     */
     Polygon& operator=(Polygon&& other) noexcept {
         if (this != &other) {
             delete[] point_array_;
@@ -60,6 +114,10 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Вычислить центр масс многоугольника
+     * @return Точка - центр масс (среднее арифметическое всех вершин)
+     */
     Point centerOfMass() const noexcept {
         float sumX = 0;
         float sumY = 0;
@@ -71,6 +129,11 @@ public:
         return Point(sumX / point_count_, sumY / point_count_);
     }
 
+    /**
+     * @brief Добавить вершину к многоугольнику
+     * @param point Новая вершина для добавления
+     * @return Ссылка на текущий объект
+     */
     Polygon& operator+=(const Point& point) {
         Point* new_point_array = new Point[point_count_ + 1];
         std::copy_n(point_array_, point_count_, new_point_array);
@@ -82,23 +145,40 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Получить доступ к вершине по индексу (неконстантная версия)
+     * @param index Индекс вершины
+     * @return Ссылка на вершину
+     */
     Point& operator[](size_t index) noexcept {
         return point_array_[index];
     }
+    
+    /**
+     * @brief Получить доступ к вершине по индексу (константная версия)
+     * @param index Индекс вершины
+     * @return Константная ссылка на вершину
+     */
     const Point& operator[](size_t index) const noexcept {
         return point_array_[index];
     }
 
+    /**
+     * @brief Повернуть многоугольник вокруг точки
+     * @param center Центр поворота
+     * @param angle Угол поворота в градусах (должен быть кратен 90°)
+     * @throws std::invalid_argument если угол не кратен 90°
+     */
     void rotate(const Point& center, size_t angle) {
         angle = angle % 360;
-        angle = angle < 0 ? angle + 360 : angle;
+ 
         if (angle % 90 != 0) {
             throw std::invalid_argument("the angle is not a multiple of 90 degrees");
         }
 
-        int steps = angle / 90;
-        for (int s = 0; s < steps; ++s) {
-            for (int i = 0; i < point_count_; ++i) {
+        size_t steps = angle / 90;
+        for (size_t s = 0; s < steps; ++s) {
+            for (size_t i = 0; i < point_count_; ++i) {
                 Point relative = point_array_[i] - center;
                 Point rotated(relative.getY(), -relative.getX());
                 point_array_[i] = rotated + center;
@@ -108,6 +188,10 @@ public:
         return;
     }
 
+    /**
+     * @brief Переместить многоугольник на заданный вектор
+     * @param vector Вектор перемещения
+     */
     void move(const Point& vector) noexcept {
         for (size_t i = 0; i < point_count_; i++) {
             point_array_[i] = point_array_[i] + vector;
@@ -116,10 +200,22 @@ public:
         return;
     }
 
+    /**
+     * @brief Получить количество вершин многоугольника
+     * @return Количество вершин
+     */
     size_t getCount() const {
         return point_count_;
     }
 
+    /**
+     * @brief Оператор вывода многоугольника в поток
+     * @param os Выходной поток
+     * @param poly Многоугольник для вывода
+     * @return Ссылка на поток
+     * 
+     * Формат вывода: количество_вершин x1 y1 x2 y2 ...
+     */
     friend std::ostream& operator<<(std::ostream& os, const Polygon& poly) noexcept {
         os << poly.point_count_ << " ";
         for (size_t i = 0; i < poly.point_count_; ++i) {
@@ -129,15 +225,23 @@ public:
         return os;
     }
 
+    /**
+     * @brief Оператор ввода многоугольника из потока
+     * @param is Входной поток
+     * @param poly Многоугольник для чтения
+     * @return Ссылка на поток
+     * 
+     * Формат ввода: количество_вершин x1 y1 x2 y2 ...
+     * Устанавливает failbit в случае ошибки чтения.
+     */
     friend std::istream& operator>>(std::istream& is, Polygon& poly) {
         try{
-            int numVertices;
+            size_t numVertices;
             is >> numVertices;
             Polygon polygon(numVertices);
             for (size_t i = 0; i < numVertices; ++i) {
                 is >> polygon.point_array_[i];
             }
-            poly = Polygon(numVertices, polygon.point_array_);
             poly = std::move(polygon);
         } catch(...) {
             is.setstate(std::ios_base::failbit);        
