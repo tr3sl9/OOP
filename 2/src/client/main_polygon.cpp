815b577a7cea21d21ff7cb64c51f3fb4aea4fc15
simonenko.mv <tr3sl9@DESKTOP-FPKAD0O.>
2025-10-16 23:39:49 +0300

    feat: add test-main file for polygon

---
+++ 2/src/client/main_polygon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "dialog.h"
+
+int main() {
+    try {
+        Dialog::run_polygon();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Произошла ошибка: " << e.what() << std::endl;
+        return 1;
+    }
+    
+    return 0;
+}
