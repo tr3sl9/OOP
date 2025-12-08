383e085cbf6d7a59b33447e2ec1d16a28554b20a
simonenko.mv <tr3sl9@DESKTOP-FPKAD0O.>
2025-12-08 21:19:56 +0300

    refactor: delete files

--- 3/src/junior_student.cpp
+++
@@ -1,9 +0,0 @@
-#include "junior_student.h"
-
-JuniorStudent::JuniorStudent(const std::string& fullname) : Student(fullname) {
-}
-
-CategoryStudent JuniorStudent::getStudentType() const {
-    return CategoryStudent::JUNIOR;
-}
-
